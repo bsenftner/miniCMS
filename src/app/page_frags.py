9bb3c01fcc08b514e6d1fbb38066528eaa037c77
Blake Senftner <bsenftner@earthlink.net>
2023-04-20 17:27:24 -0600

    header tweak

--- src/app/page_frags.py
+++ src/app/page_frags.py
@@ -5,8 +5,8 @@ FRAGS = [
 <div id="topPageHeader">
     <img id="MiniCMSLogo" src="/static/MiniCMS.png">
     <h3>MiniCMS</h3>
-    <h4> A secure project, document & info management system for in-office and remote project collaboration</h4>
-    <h5><em>for the Gloria Martinez Law Group</em></h5>
+    <h4> A secure project, document & info management system for in-office and remote project collaboration
+        for the Gloria Martinez Law Group</em></h4>
     <h5> version 1.25 </h5>
 </div>""",
 
