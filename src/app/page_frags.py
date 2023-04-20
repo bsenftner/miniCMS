0413bc4cb6b67890a4499838123682fec069dc5a
Blake Senftner <bsenftner@earthlink.net>
2023-04-20 16:29:06 -0600

    font size, sponsor ack

--- src/app/page_frags.py
+++ src/app/page_frags.py
@@ -5,8 +5,9 @@ FRAGS = [
 <div id="topPageHeader">
     <img id="MiniCMSLogo" src="/static/MiniCMS.png">
     <h3>MiniCMS</h3>
-    <h4> A secure document & info management system for in-office and remote project collaboration</h4>
-    <h5> version 1.2</h5>
+    <h4> A secure project, document & info management system for in-office and remote project collaboration</h4>
+    <h5><em>for the Gloria Martinez Law Group</em></h5>
+    <h5> version 1.25 </h5>
 </div>""",
 
         "footer": "<h5>(C) Copyright 2022 by Blake Senftner, All Rights Reserved, unless otherwise noted.</h5>",
