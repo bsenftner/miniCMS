4e52116e3932e05790de4d2877e4e100a2c3d4ff
Blake Senftner <bsenftner@earthlink.net>
2023-04-30 16:11:51 -0600

    removed unused code

--- src/app/api/aichat.py
+++ src/app/api/aichat.py
@@ -21,186 +21,6 @@ router = APIRouter()
 
 openai.api_key = get_settings().OPENAI_API_KEY
 
-""" unused # ---------------------------------------------------------------------------------------
-# create an asyncio fifo queue:
-fifo_queue = asyncio.Queue(maxsize=0)
-
-# ---------------------------------------------------------------------------------------
-# asyncio coroutine worker:
-async def asyncio_fifo_worker():
-    log.info("Starting asyncio_fifo_worker")
-    while True:
-        job = await fifo_queue.get()
-        log.info(f"asyncio_fifo_worker: job: {job})")
-        log.info(f"asyncio_fifo_worker: (size of remaining queue: {fifo_queue.qsize()})")
-        await job['runner']( job['param'] )
- """
-
-
-""" unused # ----------------------------------------------------------------------------------------------
-# new OpenAI communication as coroutine
-async def OpenAI_communication( aichat: AiChatDB ):
-    
-    log.info(f"OpenAI_communication: prompt '{aichat.prompt}'")
-    
-    prePrompt = '''You are Dr. Ernest, a bilingual English and Spanish attorney and CA Law Professor. 
-    You work for the Gloria Martinez Law Group, a Sacramento Immigration Law firm. 
-    You are meeting a potential client whom is seeking law advice. 
-    You want them to hire the firm. 
-    You only answer truthfully. 
-    If multiple options exist for the client to solve their issue, explain the client's options. 
-    If you do not have an answer, say "I do not know".
-    
-    '''
-    
-    aiResponse = None
-    if aichat.model=="text-davinci-003":        
-        aiResponse = openai.Completion.create(model=aichat.model, 
-                                              prompt= prePrompt + " \n" + aichat.prompt,
-                                              temperature=0,
-                                              max_tokens=900,
-                                              top_p=1,
-                                              frequency_penalty=0.0,
-                                              presence_penalty=0.0,
-                                        )["choices"][0]["text"].strip(" \n")
-    
-    elif aichat.model=="gpt-3.5-turbo" or aichat.model=="gpt-4":
-        aiResponse = openai.ChatCompletion.create( model=aichat.model,
-                                                   messages=[ {"role": "system", "content": prePrompt },
-                                                              {"role": "user", "content": aichat.prompt } ],
-                                                   temperature=0,
-                                                   max_tokens=900,
-                                                   top_p=1,
-                                                   frequency_penalty=0.0,
-                                                   presence_penalty=0.0,
-                                                )['choices'][0]['message']['content']
-    
-    aichat.reply = aiResponse 
-    
-    log.info(f"OpenAI_communication: reply '{aichat.reply}'")
-    
-    retVal = await crud.put_aichat( aichat )
-    
-    await crud.rememberUserAction( aichat.userid, 
-                                   UserActionLevel.index('NORMAL'),
-                                   UserAction.index('UPDATE_AICHAT'), 
-                                   f"aiChat {retVal} updated in coroutine" )
- """
-
-
-# ----------------------------------------------------------------------------------------------
-# defines an OpenAI "chat agent" instructed to compress prompts 
-class PromptCondensor:
-    def __init__(self):
-        
-        self.prePrompt = '''As a prompt condenser, you'll shorten a user prompt to its essential components without losing 
-            vital information necessary for ChatGPT to answer the condensed prompt later.'''
-        
-        self.model = "gpt-3.5-turbo"
-        
-        self.prompt = ''
-        self.reply = ''
-        
-    def condense(self,prompt):
-        self.prompt = prompt
-        self.reply = openai.ChatCompletion.create( model=self.model,
-                                                   messages=[ {"role": "system", "content": self.prePrompt },
-                                                              {"role": "user", "content": self.prompt } ],
-                                                   temperature=0,
-                                                   max_tokens=900,
-                                                   top_p=1,
-                                                   frequency_penalty=0.0,
-                                                   presence_penalty=0.0,
-                                                )['choices'][0]['message']['content']
-        return self.reply
-
-# ----------------------------------------------------------------------------------------------
-# defines an OpenAI "chat agent" instructed to compress conversations 
-class ConversationCondensor:
-    def __init__(self):
-        
-        self.prePrompt = '''As a conversation condenser, you'll shorten a conversation between a human user and a large language 
-        model to its essential components without losing vital information necessary for the output to be used as the context 
-        for later input to ChatGPT.'''
-        
-        self.model = "gpt-3.5-turbo"
-        
-        self.conversation = ''
-        self.reply = ''
-        
-    def condense(self,conversation):
-        self.conversation = conversation
-        self.reply = openai.ChatCompletion.create( model=self.model,
-                                                   messages=[ {"role": "system", "content": self.prePrompt },
-                                                              {"role": "user", "content": self.conversation } ],
-                                                   temperature=0,
-                                                   max_tokens=900,
-                                                   top_p=1,
-                                                   frequency_penalty=0.0,
-                                                   presence_penalty=0.0,
-                                                )['choices'][0]['message']['content']
-        return self.reply
-        
-
-# ----------------------------------------------------------------------------------------------
-# defines an OpenAI "chat agent" instructed to hold conversations 
-class ConversationHolder:
-    def __init__(self):
-        
-        self.prePrompt = '''As a bilingual attorney and law professor for the Gloria Martinez Law Group, I'll meet a potential client 
-            seeking law advice and truthfully answer their questions. My goal is to help the client in a step-by-step way to ensure the 
-            right answer, their understanding, and ultimately, their decision to hire our firm. If multiple options exist for the client, 
-            I'll explain each option. If I don't have an answer, I'll say "I do not know".'''
-        
-        self.model = "gpt-3.5-turbo"
-        
-        self.prompt  = ''
-        self.reply = ''
-        
-        self.promptCondensor = PromptCondensor()
-        self.conversationCondensor = ConversationCondensor()
-        
-    async def Reply(self,prompt):
-        
-        log.info(" ")
-        log.info(f"Conversation.Reply: entered with prePrompt '{self.prePrompt}'")
-        
-        log.info(" ")
-        log.info(f"Conversation.Reply: entered with prompt '{prompt}'")
-        
-        self.prompt = await asyncio.to_thread(self.promptCondensor.condense, prompt)
-        
-        log.info(" ")
-        log.info(f"Conversation.Reply: condensed prompt '{self.prompt}'")
-        
-        self.reply = openai.ChatCompletion.create( model=self.model,
-                                                   messages=[ {"role": "system", "content": self.prePrompt },
-                                                              {"role": "user", "content": self.prompt } ],
-                                                   temperature=0,
-                                                   max_tokens=900,
-                                                   top_p=1,
-                                                   frequency_penalty=0.0,
-                                                   presence_penalty=0.0,
-                                                )['choices'][0]['message']['content']
-        
-        log.info(" ")
-        log.info(f"Conversation.Reply: reply '{self.reply}'")
-        
-        bsjnk = self.prePrompt + "\n" + self.prompt + "\n" + self.reply
-        
-        log.info(" ")
-        log.info(f"Conversation.Reply: going to compress '{bsjnk}'")
-        
-        self.prePrompt = await asyncio.to_thread(self.conversationCondensor.condense, bsjnk )
-        
-        log.info(" ")
-        log.info(f"Conversation.Reply: condensed context '{self.prePrompt}'")
-        
-        log.info(" ")
-        log.info(f"Conversation.Reply: reply '{self.reply}'")
-        
-        return self.reply
-        
 # ----------------------------------------------------------------------------------------------
 # new OpenAI communication as separate thread
 def OpenAI_thread_comm( aichat: AiChatDB ):
@@ -245,17 +65,6 @@ def OpenAI_thread_comm( aichat: AiChatDB ):
     
     log.info(f"OpenAI_thread_comm: reply '{aichat.reply}'")
     
-    
-'''
-older methods no longer used 
-
-    # this method (commented out) runs with a coroutine:
-    # await fifo_queue.put({ "runner": OpenAI_communication, "param": aichat}) 
-    #
-    # this version runs in a separate thead, requiring the following two lines 
-    # the other method does in its coroutine
-    await asyncio.to_thread(OpenAI_thread_comm, aichat)
-'''
 # ----------------------------------------------------------------------------------------------
 # declare a POST endpoint on the root 
 @router.post("/", response_model=AiChatCreateResponse, status_code=201)
@@ -310,31 +119,7 @@ async def create_aiChatExchange(payload: AiChatCreate,
     #
     aichat: AiChatDB = await crud.get_aiChat( aichatid )
     
-    if (True):
-        await asyncio.to_thread(OpenAI_thread_comm, aichat)
-    else:
-        log.info(f"create_aiChatExchange: creating the conversation object...")
-        
-        # this creates a "conversation holder" that both condenses user prompts 
-        # before passing them to the GPT model, as well as condenses the entire 
-        # conversation and puts that in the prePrompt field for future use as    
-        # the conversation's context: 
-        conversation = ConversationHolder()
-        #
-        # conversation.Reply() handles the above described condensing, GPT asking 
-        # and condensing again: 
-        #
-        log.info(f"create_aiChatExchange: asking...")
-        # 
-        # asyncio.run(my_coroutine())
-        #
-        # aichat.reply = await asyncio.to_thread(conversation.Reply, aichat.prompt)
-        # aichat.reply = asyncio.run( conversation.Reply( aichat.prompt ))
-        aichat.reply = await conversation.Reply( aichat.prompt )
-        # 
-        # remember the entire conversation for use as the context next question: 
-        aichat.prePrompt = conversation.prePrompt # also calculated by conversation.Reply()
-    
+    await asyncio.to_thread(OpenAI_thread_comm, aichat)
     
     retVal = await crud.put_aichat( aichat )
     
@@ -487,21 +272,11 @@ async def update_aiChatExchange(payload: basicTextPayload,       # a new questio
                                        "Not Authorized" )
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not Authorized to access project.")
         
-    if True:
-        aichat.prompt = aichat.prompt + "<br><br>" + aichat.reply + "<br><br>" + payload.text
-        aichat.reply = ''
-        await asyncio.to_thread(OpenAI_thread_comm, aichat )
-    else:
-        # start with a generic conversation:
-        conversation = ConversationHolder()
-        # take any prior conversation's context and put that in the prePrompt: 
-        conversation.prePrompt += "\n " + aichat.prePrompt
-        # put the new question here: 
-        aichat.prompt = payload.text
-        # ask the question, get the reply:
-        aichat.reply = await conversation.Reply( aichat.prompt )
-        # remember the entire conversation for use as the context next question: 
-        aichat.prePrompt = conversation.prePrompt # also calculated by conversation.Reply()
+
+    aichat.prompt = aichat.prompt + "<br><br>" + aichat.reply + "<br><br>" + payload.text
+    aichat.reply = ''
+    await asyncio.to_thread(OpenAI_thread_comm, aichat )
+
     
     retVal = await crud.put_aichat( aichat )
     
